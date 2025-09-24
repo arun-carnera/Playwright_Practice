ba341965f9c98ffcf363c6c1405132f427274d3f
arun-carnera <your-email@example.com>
2025-09-24 15:36:00 +0530

    Excel utilities added

--- tests/inputs.spec.js
+++ tests/inputs.spec.js
@@ -1,11 +1,17 @@
 import {test, expect} from '@playwright/test';
 import Input from '../pages/inputs.page';
 import {navigate} from '../utils/navigate.utils';
+import {getExcelSheetData} from '../utils/excel.utils';
+import path from 'path';
 
 test("Input", async({page}) =>{
     const input = new Input(page);
-    await navigate(page, process.env.HOMEPAGE_URL, input.PAGE_LINK, input.HEADER);
+    const excelDataArr = getExcelSheetData((path.join(__dirname, '../testdata/playwright_excel.xlsx')), 'dev');
+    console.log(excelDataArr);
+    await page.goto(excelDataArr[0].URL);
+    await input.PAGE_LINK.click();
+    await expect(input.HEADER).toBeVisible();
     await input.INPUT.click();
     await page.keyboard.press('ArrowUp');
     await expect(input.INPUT).toHaveValue("1");
-})
+});
