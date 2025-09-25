4316f1d7444605cf7ac944ab2991f26162e8a914
kaushal-carnera <kaushal@getcarnera.com>
2025-09-25 20:40:09 +0530

    failed test recoded

--- playwright.config.js
+++ playwright.config.js
@@ -40,6 +40,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.HOMEPAGE_URL,
+    permissions: ['geolocation'],
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- tests/geoLocation.spec.js
+++ tests/geoLocation.spec.js
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
 import GeoLocation from '../pages/geoLocation.page';
 
+test.use({
+  permission : ['geolocation'],
+  geolocation: { longitude: 41.890221, latitude: 12.492348 },
+})
+
 test('Geolocation Test', async ({page}) => {
   const geoLocationPage = new GeoLocation(page);
   await geoLocationPage.navigate();
@@ -8,7 +13,13 @@ test('Geolocation Test', async ({page}) => {
   await expect(geoLocationPage.PAGE_SUB_HEADER).toBeVisible();
   await geoLocationPage.GET_LOCATION_BUTTON.click();
   await expect(geoLocationPage.LAT_VALUE).toBeVisible();
+  const latValue = await geoLocationPage.LAT_VALUE.textContent();
+  console.log("Latitude Value:", latValue);
+  await expect(geoLocationPage.LAT_VALUE).toHaveText("12.492348");
   await expect(geoLocationPage.LONG_VALUE).toBeVisible();
+  const longValue = await geoLocationPage.LONG_VALUE.textContent();
+  console.log("Longitude Value:", longValue);
+  await expect(geoLocationPage.LONG_VALUE).toHaveText("41.890221");
   await expect(geoLocationPage.LONG_HEADING).toBeVisible();
   await expect(geoLocationPage.LAT_HEADING).toBeVisible();
   await page.waitForTimeout(2000);
